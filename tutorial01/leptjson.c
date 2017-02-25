8a59d1cd8d39eb7821a5076fd0854d165076937d
jenenliu <jenenliu@gmail.com>
2017-02-25 17:32:57 +0800

    add lept_parse_true and lept_parse_false unit test and impl.

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -2,6 +2,7 @@
 #include <assert.h>  /* assert() */
 #include <stdlib.h>  /* NULL */
 
+/* first character of lept_context equal to c */
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
 
 typedef struct {
@@ -10,12 +11,14 @@ typedef struct {
 
 static void lept_parse_whitespace(lept_context* c) {
     const char *p = c->json;
+    /* ignore whitespace characters */
     while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
         p++;
     c->json = p;
 }
 
 static int lept_parse_null(lept_context* c, lept_value* v) {
+  /* if the first character of c.json is not 'n', then warn */
     EXPECT(c, 'n');
     if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
         return LEPT_PARSE_INVALID_VALUE;
@@ -24,9 +27,30 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_true(lept_context* c, lept_value* v) {
+  EXPECT(c, 't');
+  if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+    return LEPT_PARSE_INVALID_VALUE;
+  c->json += 3;
+  v->type = LEPT_TRUE;
+  return LEPT_PARSE_OK;
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+  EXPECT(c, 'f');
+  if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's'
+      || c->json[3] != 'e')
+    return LEPT_PARSE_INVALID_VALUE;
+  c->json += 4;
+  v->type = LEPT_FALSE;
+  return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 't':  return lept_parse_true(c, v);
+        case 'f':  return lept_parse_false(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
